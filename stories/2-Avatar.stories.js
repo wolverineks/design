98615a425c1d977b089225e8bb53f7730e6d710d
Michael Chan <mijoch@gmail.com>
2019-10-03 08:51:09 -0700

    WIP

---
+++ stories/2-Avatar.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Avatar } from "../planningcenter/avatar/src/avatar";
+// import { action } from '@storybook/addon-actions';
+// import { Button } from '@storybook/react/demo';
+
+export default {
+  title: "Avatar"
+};
+
+// export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+
+export const emoji = () => (
+  <Avatar />
+);
